589675226cfd2803f41defc3cdf18df8d2e33cd9
Kevin <ksrbkevinsuen@gmail.com>
2014-10-12 06:04:07 -0400

    Basic button transition with beat validation complete

--- public/js/main.js
+++ public/js/main.js
@@ -5,24 +5,43 @@ $(function () {
     var socket = io();
 
     var beatsButton = $("#beats-button");
-    var beatsButtonToggled = false;
+    var beatsButtonClickable = false;
 
     var toggleBeatsButton = function () {
-        if (beatsButtonToggled) {
-            beatsButton.removeClass("btn-primary");
-            beatsButton.addClass("btn-danger");
-            beatsButtonToggled = false;
-        } else {
+        if (beatsButtonClickable) {
+            beatsButtonClickable = false;
             beatsButton.addClass("btn-primary");
             beatsButton.removeClass("btn-danger");
-            beatsButtonToggled = true;
+        } else {
+            beatsButtonClickable = true;
+            beatsButton.removeClass("btn-primary");
+            beatsButton.addClass("btn-danger");
         }
     }
 
-    beatsButton.click(toggleBeatsButton);
+    var beatsButtonClickHandler = function () {
+        if (beatsButtonClickable == true) {
+            beatsButton.unbind('click', beatsButtonClickHandler);
+            socket.emit("send score", {success: true});
+            console.log("Beat hit");
+        }
+    }
+
+    beatsButton.click(beatsButtonClickHandler);
 
     socket.on('action signal', function (data) {
         toggleBeatsButton();
+
+        setTimeout(function () {
+            toggleBeatsButton();
+            beatsButton.unbind('click', beatsButtonClickHandler);
+            beatsButton.click(beatsButtonClickHandler);
+        }, data.interval);
     });
+
+    var showTime = function () {
+        var date = new Date();
+        console.log(date.getSeconds());
+    }
 
 });
